b93df6167b8551150a5c86b124c2c2565cb463cc
lazukr <peterh10201@gmail.com>
2017-06-04 00:38:50 +0200

    convert tabs into spaces

--- main.py
+++ main.py
@@ -7,75 +7,75 @@ SCREEN_SIZE = (640, 480)
 GENERATION_TRIES = 100
 
 class Resoursa():
-	def __init__(self):
+    def __init__(self):
 
-		# initialization
-		pygame.display.init()
-		pygame.font.init()
+        # initialization
+        pygame.display.init()
+        pygame.font.init()
 
-		self.screen = pygame.display.set_mode(SCREEN_SIZE)
-		pygame.display.set_caption("Resoursa")
+        self.screen = pygame.display.set_mode(SCREEN_SIZE)
+        pygame.display.set_caption("Resoursa")
 
-		self.items = {
-			"count": 0,
-			"list": []
-		}
+        self.items = {
+            "count": 0,
+            "list": []
+        }
 
-		self.clock = pygame.time.Clock()
+        self.clock = pygame.time.Clock()
 
-		self.world_start()
-		
-
-
-
-	def world_start(self):
-		generation_parameters = {}
-		with open("world_gen.txt") as f:
-			for line in f:
-				(name, probability) = line.split()
-				generation_parameters[name] =  int(float(probability)*GENERATION_TRIES)
-		print(generation_parameters)
-
-
-		for materials, amount in generation_parameters.items():
-			print(materials, amount)
-			for x in range(amount):
-				location = (randint(0, SCREEN_SIZE[0]), randint(0, SCREEN_SIZE[1]))
-				self.items["list"].append((Wood(self.items["count"], materials),
-							 location))
-				self.items["count"] += 1
-
-
-		print(self.items)
+        self.world_start()
+        
 
 
 
-	def draw_screen(self):
-		display_objects = self.items["list"]
-		for elements in display_objects:
-			#print(elements)
-			self.screen.blit(elements[0].image, elements[1])
+    def world_start(self):
+        generation_parameters = {}
+        with open("world_gen.txt") as f:
+            for line in f:
+                (name, probability) = line.split()
+                generation_parameters[name] =  int(float(probability)*GENERATION_TRIES)
+        print generation_parameters
 
 
-	def update(self):
+        for materials, amount in generation_parameters.items():
+            print materials, amount
+            for x in range(amount):
+                location = (randint(0, SCREEN_SIZE[0]), randint(0, SCREEN_SIZE[1]))
+                self.items["list"].append((Wood(self.items["count"], materials),
+                             location))
+                self.items["count"] += 1
 
-		# maintain 60 hz
-		self.clock.tick(60)
-		self.screen.fill((0, 255, 255))
-		self.draw_screen()
 
-		mouse = pygame.mouse.get_pos()
+        print(self.items)
 
 
-		for event in pygame.event.get():
 
-			if event.type == pygame.QUIT:
-				exit()
+    def draw_screen(self):
+        display_objects = self.items["list"]
+        for elements in display_objects:
+            #print(elements)
+            self.screen.blit(elements[0].image, elements[1])
 
-		# updates screen
-		pygame.display.flip()
+
+    def update(self):
+
+        # maintain 60 hz
+        self.clock.tick(60)
+        self.screen.fill((0, 255, 255))
+        self.draw_screen()
+
+        mouse = pygame.mouse.get_pos()
+
+
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                exit()
+
+        # updates screen
+        pygame.display.flip()
 
 
 resoursa = Resoursa()
 while True:
-	resoursa.update()
+    resoursa.update()
--- materials.py
+++ materials.py
@@ -3,37 +3,37 @@
 import pygame
 
 MATERIALS_WOOD = {
-	"Oak": {
-		"size": ((5, 23)),
-		"width_deviation": 0,
-		"height_deviation": 1,
-		"colour": (79, 36, 18)
-	},
-	"Birch": {
-		"size": ((3, 12)),
-		"width_deviation": 1,
-		"height_deviation": 2,
-		"colour": (255, 153, 0)
-	},
-	"Bloodred": {
-		"size": ((15, 3)),
-		"colour": (255, 25, 25)
-	}
+    "Oak": {
+        "size": ((5, 23)),
+        "width_deviation": 0,
+        "height_deviation": 1,
+        "colour": (79, 36, 18)
+    },
+    "Birch": {
+        "size": ((3, 12)),
+        "width_deviation": 1,
+        "height_deviation": 2,
+        "colour": (255, 153, 0)
+    },
+    "Bloodred": {
+        "size": ((15, 3)),
+        "colour": (255, 25, 25)
+    }
 }
 
 
 def rect(self, size, colour):
-	surface = pygame.Surface(size)
-	surface.fill(colour)
-	return surface
+    surface = pygame.Surface(size)
+    surface.fill(colour)
+    return surface
 
 
 
 class Wood():
-	def __init__(self, unique_id, material):
-		self.id = unique_id
-		self.material = material
-		self.size = MATERIALS_WOOD[material]["size"]
-		self.colour = MATERIALS_WOOD[material]["colour"]
-		self.image = rect(self, self.size, self.colour)
+    def __init__(self, unique_id, material):
+        self.id = unique_id
+        self.material = material
+        self.size = MATERIALS_WOOD[material]["size"]
+        self.colour = MATERIALS_WOOD[material]["colour"]
+        self.image = rect(self, self.size, self.colour)
 
