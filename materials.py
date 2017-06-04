e970d983073b02a9f329e4fdfa32356127c4702e
lazukr <peterh10201@gmail.com>
2017-06-04 03:26:08 +0200

    Created matreial_properties.txt which acts as a table for all the properties of fundamental materials. materials.py now reads from it to find all the property keys and generate a dictionary for all the materials. The Wood class has also been modified to accomodate the scalability.

--- materials.py
+++ materials.py
@@ -1,25 +1,26 @@
 # materials.py contains all the outline
 # to the fundamentals of all materials
 import pygame
+import ast
 
-MATERIALS_WOOD = {
-    "Oak": {
-        "size": ((5, 23)),
-        "width_deviation": 0,
-        "height_deviation": 1,
-        "colour": (79, 36, 18)
-    },
-    "Birch": {
-        "size": ((3, 12)),
-        "width_deviation": 1,
-        "height_deviation": 2,
-        "colour": (255, 153, 0)
-    },
-    "Bloodred": {
-        "size": ((15, 3)),
-        "colour": (255, 25, 25)
-    }
-}
+# dictionary for materials
+MATERIALS_WOOD = {}
+
+# keys to properties
+PROPERTY_KEYS = []
+with open("material_properties.txt") as f:
+    for line in f:
+
+        # detect all the properties
+        if line.startswith("$"):
+            this_line = line[2:].split(',')           
+            for elements in [[x.strip() for x in properties.split('=')] for properties in this_line]:
+                PROPERTY_KEYS.append(elements[1])
+
+        # detect all the materials
+        elif not line.startswith("#"):
+            this_line = line.split()
+            MATERIALS_WOOD[this_line[0]] = dict(zip(PROPERTY_KEYS[1:], this_line[1:]))
 
 
 def rect(self, size, colour):
@@ -33,7 +34,13 @@ class Wood():
     def __init__(self, unique_id, material):
         self.id = unique_id
         self.material = material
-        self.size = MATERIALS_WOOD[material]["size"]
-        self.colour = MATERIALS_WOOD[material]["colour"]
-        self.image = rect(self, self.size, self.colour)
+        self.properties = {}
+        for properties in PROPERTY_KEYS[1:]:
+            self.properties[properties] = ast.literal_eval(MATERIALS_WOOD[material][properties])
+        self.image = rect(self, self.properties["size"], self.properties["colour"])
 
+
+
+if __name__ == "__main__":
+    aWood = Wood(1, "Oak")
+    print vars(aWood)
