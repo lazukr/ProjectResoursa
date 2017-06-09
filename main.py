0d92ec46bf3f8b7dc49236032b6a66d6500d46a0
lazukr <peterh10201@gmail.com>
2017-06-09 05:55:44 +0200

    incorporated various thorpy elements into pygame. Needed to borrow code inside the menu class and manually run it on the event loop in order to get hoverables functional.

--- main.py
+++ main.py
@@ -1,32 +1,54 @@
-import pygame
-from materials import Wood
+import pygame, thorpy
+from materials import Metal, MATERIALS
 from random import randint
 import math
 
 SCREEN_SIZE = (640, 480)
 GENERATION_TRIES = 100
 
-class Resoursa():
-    def __init__(self):
-
-        # initialization
-        pygame.display.init()
-        pygame.font.init()
-
-        self.screen = pygame.display.set_mode(SCREEN_SIZE)
-        pygame.display.set_caption("Resoursa")
-
+class Game(object):
+    def __init__(self, dim=SCREEN_SIZE, fps=30):
+        self.dim = dim
+        self.fps = fps
+        self.clock = pygame.time.Clock()
+        self.screen = None
         self.items = {
             "count": 0,
             "list": []
         }
+        self.inventory = []
+        self.max_inv = 5
 
-        self.clock = pygame.time.Clock()
+    def start(self):
+        pygame.display.init()
+        pygame.font.init()
+        self.screen = pygame.display.set_mode(self.dim)
+        self.first_render()
+        is_running = True
+        while is_running:
+            self.clock.tick(self.fps)
+            tick = self.clock.get_time()
+            this_event = pygame.event.Event(thorpy.constants.THORPY_EVENT,
+            id=thorpy.constants.EVENT_TIME, tick=tick)
+            pygame.event.post(this_event)
 
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    is_running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    self.mouse_down()
+                self.on_event(event)
+            self.render()
+            pygame.display.flip()
+        pygame.quit()
+
+class Resoursa(Game):
+    def __init__(self):
+
+        # initialization
+        super(Resoursa, self).__init__()
+        pygame.display.set_caption("Resoursa")
         self.world_start()
-        
-
-
 
     def world_start(self):
         generation_parameters = {}
@@ -34,48 +56,95 @@ class Resoursa():
             for line in f:
                 (name, probability) = line.split()
                 generation_parameters[name] =  int(float(probability)*GENERATION_TRIES)
-        print generation_parameters
-
+        # print generation_parameters
 
         for materials, amount in generation_parameters.items():
             print materials, amount
             for x in range(amount):
                 location = (randint(0, SCREEN_SIZE[0]), randint(0, SCREEN_SIZE[1]))
-                self.items["list"].append((Wood(self.items["count"], materials),
-                             location))
+                this_item = Metal(self.items["count"], materials)
+                this_item.image.move_ip(location)
+                self.items["list"].append(this_item)
                 self.items["count"] += 1
+        # print(self.items)
+
+        
 
 
-        print(self.items)
+    def combine(self):
+        items = [checker.get_text() for checker in self.checkers if checker.get_value() ]
+        print 'Combining: {}'.format(items)
 
-
-
-    def draw_screen(self):
+    def draw_items(self):
         display_objects = self.items["list"]
         for elements in display_objects:
-            #print(elements)
-            self.screen.blit(elements[0].image, elements[1])
+            pygame.draw.rect(self.screen, elements.properties["colour"], elements.image)
+
+    def draw_gui(self):
+        self.checkers = [thorpy.Checker.make(resource, value=False) for resource in MATERIALS.keys()]
+        self.inv_display = [thorpy.Clickable.make("None") for x in range(self.max_inv)]
+        button = thorpy.make_button("Combine", func=self.combine)
+        elements = []
+        elements += self.checkers 
+        elements.append(button)
+        self.inv_box = thorpy.Box.make(elements=self.inv_display)
+        self.inv_box.fit_children(margins=(10,10))
+        self.inv_drag = thorpy.Draggable.make(elements=[self.inv_box])
+
+        self.box = thorpy.Box.make(elements=elements)
+        self.menu = thorpy.Menu(self.box)
+        self.inv_menu = thorpy.Menu(self.inv_drag)
 
 
-    def update(self):
+        for element in self.menu.get_population():
+            element.surface = self.screen
 
-        # maintain 60 hz
-        self.clock.tick(60)
-        self.screen.fill((0, 255, 255))
-        self.draw_screen()
+        for element in self.inv_menu.get_population():
+            element.surface = self.screen
 
-        mouse = pygame.mouse.get_pos()
+        self.box.set_topleft((0,0))
+        self.box.blit()
+        self.box.update()
 
+        self.inv_drag.set_topleft((100,100))
+        self.inv_drag.blit()
+        self.inv_drag.update()
 
-        for event in pygame.event.get():
-
-            if event.type == pygame.QUIT:
-                exit()
-
-        # updates screen
-        pygame.display.flip()
+        thorpy.functions.set_current_menu(self.menu)
+        thorpy.functions.set_current_menu(self.inv_menu)
 
 
-resoursa = Resoursa()
-while True:
-    resoursa.update()
+
+    def first_render(self):
+        self.draw_items()
+        self.draw_gui()
+
+    def render(self):
+        self.screen.fill((255, 255, 255))
+        self.draw_items()
+        self.inv_drag.unblit_and_reblit()
+        self.box.unblit_and_reblit()
+        
+
+    def on_event(self, event):
+        self.menu.react(event)
+        self.inv_menu.react(event)
+
+    def mouse_down(self):
+        mw, mh = pygame.mouse.get_pos()
+        for elements in self.items["list"]:
+            if elements.image.collidepoint(mw, mh):
+                print elements
+                cur_inv_stock = len(self.inventory)
+                if cur_inv_stock < self.max_inv:
+                    self.inventory.append(elements)
+                    self.inv_display[cur_inv_stock].set_text(str(elements.id))
+                    self.inv_display[cur_inv_stock].add_basic_help(elements.get_property_str())
+
+
+
+
+if __name__ == "__main__":
+    resoursa = Resoursa()
+    resoursa.start()
+
--- materials.py
+++ materials.py
@@ -2,9 +2,11 @@
 # to the fundamentals of all materials
 import pygame
 import ast
+import decimal as Decimal
+from random import randint
 
 # dictionary for materials
-MATERIALS_WOOD = {}
+MATERIALS = {}
 
 # keys to properties
 PROPERTY_KEYS = []
@@ -17,30 +19,86 @@ with open("material_properties.txt") as f:
             for elements in [[x.strip() for x in properties.split('=')] for properties in this_line]:
                 PROPERTY_KEYS.append(elements[1])
 
+
+        elif line.startswith("%"):
+            pass
+        elif line.startswith("&"):
+            pass
         # detect all the materials
         elif not line.startswith("#"):
             this_line = line.split()
-            MATERIALS_WOOD[this_line[0]] = dict(zip(PROPERTY_KEYS[1:], this_line[1:]))
+            MATERIALS[this_line[0]] = dict(zip(PROPERTY_KEYS[1:], this_line[1:]))
 
 
-def rect(self, size, colour):
-    surface = pygame.Surface(size)
-    surface.fill(colour)
-    return surface
+# def rect(self, size):
+#     width, height = size
+#     this_rect = pygame.Rect((0, 0, width, height))
+#     return this_rect
 
 
 
-class Wood():
-    def __init__(self, unique_id, material):
+class Material(object):
+    def __init__(self, unique_id, material_class, material):
         self.id = unique_id
+        self.type = material_class
         self.material = material
+        self.temperature = 300
         self.properties = {}
         for properties in PROPERTY_KEYS[1:]:
-            self.properties[properties] = ast.literal_eval(MATERIALS_WOOD[material][properties])
-        self.image = rect(self, self.properties["size"], self.properties["colour"])
+            self.properties[properties] = ast.literal_eval(MATERIALS[material][properties])
+        w, h = self.properties["size"]
+        w_dev_max, h_dev_max = self.properties["size_deviation"]
+        w_dev = randint(0, w_dev_max)
+        h_dev = randint(0, h_dev_max)
+        self.properties["size"] = (w + w_dev, h + h_dev)
+        self.volume = self.properties["size"][0]* self.properties["size"][1]
+        self.image = pygame.Rect((0, 0), self.properties["size"])
+
+
+    def __str__(self):
+        return ("Id: %s, Type: %s, Temperature: %s, Volume: %s \nProperties: %s" %
+                 (self.id, self.type, self.temperature, self.volume, self.properties))
+
+
+    def get_property_str(self):
+        return ("Material Class: %s\nMaterial Name: %s\nTemperature: %s\nVolume: %s" %
+                (self.type, self.material, self.temperature, self.volume))
+
+
+
+
+# class Wood(Material):
+#     def __init__(self, unique_id, material):
+#         super(Wood, self).__init__(unique_id, "Wood", material)
+
+
+class Metal(Material):
+    def __init__(self, unique_id, material):
+        super(Metal, self).__init__(unique_id, "Metal", material)
+
+
+
+# class Alloy(Metal):
+#     def __init__(self, metal_1, metal_2):
+#         total_volume = metal_1.volume + metal_2.volume
+#         metal_1_ratio = round(Decimal(metal_1.volume / total_volume), 4)
+#         metal_2_ratio = round(Decimal(1 - metal_1_ratio), 4)
+#         name = metal_1.name + str(metal_1_ratio) + "-" + metal_2.name + str(metal_2_ratio)
+#         melt_affinity = (metal_1.melt_affinity + metal_2.melt_affinity) / 2
+#         melting_point = Decimal(metal_1.melting_point)*(Decimal(metal_1_ratio) ** Decimal(metal_1.melt_affinity)) \
+#                      + Decimal(metal_2.melting_point)*(1 - Decimal(metal_1_ratio) ** Decimal(metal_2.melt_affinity)) 
+#         super(Alloy, self).__init__(name, melt_affinity, melting_point, total_volume)
+
+
 
 
 
 if __name__ == "__main__":
-    aWood = Wood(1, "Oak")
-    print vars(aWood)
+    all_materials = []
+    counter = 0
+    for x in MATERIALS:
+        all_materials.append(Material(counter, x))
+        counter += 1
+
+    for materials in all_materials:
+        print vars(materials)
